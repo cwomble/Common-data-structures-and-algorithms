f7dc5c95ccadb7f9b05eb49d9d969d08e28ad492
cwomble <womble.charles@gmail.com>
2014-12-05 20:26:28 -0600

    Implemented a basic stack
    implemented a basic stack and added information about it. Todo: comment
    extra helpful information

--- src/dataStructures/Stack.java
+++ src/dataStructures/Stack.java
@@ -1,8 +1,62 @@
 package dataStructures;
 
+/**
+ * @author cwomble
+ * The stack is a last in first out data structure.
+ * It is a list of objects with a pointer only to the top.
+ * like a queue it is very easy to implement with a linked list.
+ * Can be used for things like recursion, backtracking, and memory management.
+ * Complexity: O(1) for push pop isEmpty
+ * Pros:
+ * fast and easy to implement made with array
+ * fast and grow shrink dynamically made with linked list
+ * Cons:
+ * resizing the stack is O(n) made with array
+ * allocating/freeing memory can be costly made with linked list
+ * Types:
+ * Plain stack, array or linked list implementation.
+ *
+ */
 public class Stack 
 {
-
+	Node top = null;
 	
+	public Node pop()
+	{
+		if (top != null)
+		{
+			Node oldTop = top;
+			top = top.previous;
+			return oldTop;
+		}
+		return null;
+	}
 	
+	public void push(Node newNode)
+	{
+		if (top != null)
+		{
+			newNode.previous = top;
+			top = newNode;
+		}
+		else top = newNode;
+	}
+	
+	public Node peek()
+	{
+		return top;
+	}
+	
+	public boolean isEmpty()
+	{
+		if (top == null)
+			return true;
+		return false;
+	}
+	
+	public class Node
+	{
+		int intData;
+		Node previous = null;
+	}
 }
